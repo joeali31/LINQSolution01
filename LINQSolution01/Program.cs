ed6b72ee1f087403108cbac820e414baa36a489d
joeali31 <ya9415565@gmail.com>
2026-04-10 07:56:57 -0700

    Question04

--- LINQSolution01/Program.cs
+++ LINQSolution01/Program.cs
@@ -42,6 +42,16 @@ namespace LINQSolution01
             #endregion
 
 
+            #region Question04
+            // Get all products where UnitPrice is between 10 and 30
+
+            //var products = Source.ProductList.Where(p => p.UnitPrice >= 10 && p.UnitPrice <= 30).Select(p => p);
+
+            //foreach (var item in products)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
 
         }
     }
