1b7811e96afd097ec872211fe1c41e62d15ec741
joeali31 <ya9415565@gmail.com>
2026-04-10 07:33:28 -0700

    Add project files.

---
+++ LINQSolution01/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQSolution01.Models
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Grade { get; set; }
+        public string Major { get; set; }
+    }
+}
---
+++ LINQSolution01/Program.cs
@@ -0,0 +1,13 @@
+﻿using LINQSolution01.DataSources;
+using LINQSolution01.Models;
+
+namespace LINQSolution01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
